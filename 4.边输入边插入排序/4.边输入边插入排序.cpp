c2b2d659a842d9c3793f4bdb4816ef64a25d0bf3
HClhsl <75610979+HClhsl@users.noreply.github.com>
2020-12-07 17:51:09 +0800

    添加项目文件。

---
+++ 4.边输入边插入排序/4.边输入边插入排序.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int x,i=1,a[100],t;
+	cout<< "请输入整数（输零结束）";
+	cin >> a[0];
+	x = a[0];
+	while (x)
+	{
+		cout << "请输入整数（输零结束）";
+		cin >> x;
+		a[i] = x;
+		for(int m=i-1;m>=0;m--)
+			for(int p=i;p>=0;p--)
+			if (a[p] < a[m])
+			{
+				t = a[m];
+				a[m] = a[p];
+				a[p] = t;
+			}
+	for (int n = 0; n <= i; n++)
+		cout << a[n] << '\t';
+	cout << endl;
+	i += 1;
+	}
+	return 0;
+}
